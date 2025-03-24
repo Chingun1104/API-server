e88342f298cda13eedc05fa11547532d74852fa2
24LP6392 24LP6392 <24LP6392@G303-CC0155.local>
2025-03-24 12:40:57 +0800

    first

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express  , { Request, Response } from 'express';
+
+const app = express();
+
+const port = 3000;
+app.get('/', (req : Request, res : Response ) => {
+    const food = [];
+    {
+        sucess : true
+    }
+    res.json(food)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running port ${port} `) ;
+
+})
